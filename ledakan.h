f179c350f1a1be94158a7a8130ad9cbdbd9947e7
steve1994 <steve.harnadi@gmail.com>
2015-02-22 19:07:38 +0700

    Ledakan polygon sementara

---
+++ ledakan.h
@@ -0,0 +1,35 @@
+#ifndef _LEDAKAN_H
+#define _LEDAKAN_H
+
+#define PI 3.14159265
+
+#include "garis.h"
+#include <stdio.h>
+#include <math.h>
+
+class Ledakan {
+	public :
+		Ledakan(int posisiX, int posisiY);				// ctor
+		Ledakan();										// ctor default
+		~Ledakan();										// dtor
+		
+		void drawLedakan();								// prosedur untuk menggambar ledakan sesaat
+		void clearLedakan();							// prosedur untuk menghapus gambar ledakan sesaat
+		void meledak();									// prosedur untuk menggambar ledakan secara keseluruhan dari awal sampai akhir
+		int absisHasilTrigonometri(double degrees);		// fungsi untuk menghitung proyeksi radius ledakan pada sumbu X dengan sudut degrees
+		int ordinatHasilTrigonometri(double degrees);	// fungsi untuk menghitung proyeksi radius ledakan pada sumbu Y dengan sudut degrees
+		
+		// GETTER DAN SETTER TITIK LEDAKAN
+		void setAbsisTitikPusatLedakan (int newAbsis);
+		void setOrdinatTitikPusatLedakan (int newOrdinat);
+		void setRadiusLedakan (int newRadius);
+		int getAbsisTitikPusatLedakan();
+		int getOrdinatTitikPusatLedakan();
+		int getRadiusLedakan ();
+	private :
+		Point titikPusatLedakan;
+		int radiusLedakan;
+		Buffer frameBuffer;
+};
+
+#endif
