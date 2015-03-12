4306d561204d057543e922b00f28450d95528718
Hendro Triokta Brianto <hendro@hendro-VirtualBox.(none)>
2015-03-12 09:11:43 +0700

    change all file

--- heli_kapal/mledakan.cpp
+++
@@ -1,6 +0,0 @@
-#include "ledakan.h"
-
-int main () {
-	Ledakan ledakanMaut(500,500);
-	ledakanMaut.meledak();
-}
--- heli_kapal/mpeluru.cpp
+++
@@ -1,6 +0,0 @@
-#include "peluru.h"
-
-int main() {
-	Peluru misil(100,700);
-	misil.luncurkanPeluru(100);
-}
